0159289a8e11b383862dc913f86a84e54c353b5b
Ravi Hasija <ravi.hasija@charter.com>
2018-10-21 02:22:03 -0700

    Remove CompileStatic from PersonService interface

--- micronaut-gorm/src/main/groovy/micronaut/gorm/service/PersonService.groovy
+++ micronaut-gorm/src/main/groovy/micronaut/gorm/service/PersonService.groovy
@@ -1,14 +1,11 @@
 package micronaut.gorm.service
 
-
 import grails.gorm.services.Service
-import groovy.transform.CompileStatic
 import micronaut.gorm.domain.Person
 
 import javax.validation.constraints.NotNull
 
 @Service(Person)
-@CompileStatic
 interface PersonService {
 
     int count()
